df8dae737f8b049e39dfc08e7963721a84361859
Mbelenga <138711355+Mbelenga@users.noreply.github.com>
2024-01-24 01:48:10 +0300

    Update 0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -6,24 +6,4 @@
  * @size: the size of the array
  * Return: the sorted array
  */
-void bubble_sort(int *array, size_t size)
-{
-	size_t j, index, temp;
-
-	if (array == NULL || size < 2)
-		return;
-
-	for (index = 0; index < size - 1; index++)
-	{
-		for (j = 0; j < size - index - 1; j++)
-		{
-			if (array[j] > array[j + 1])
-			{
-				temp = array[j];
-				array[j] = array[j + 1];
-				array[j + 1] = temp;
-				print_array(array, size);
-			}
-		}
-	}
-}
+void bubble_sort(int *array, size_t size
